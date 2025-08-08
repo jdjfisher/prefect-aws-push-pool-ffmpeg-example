71e3b2fc31874d6c166d637850e3c8e6d77e1f47
Jordan Fisher <jordan.fisher@vivedia.net>
2025-08-08 16:49:44 +0100

    deployed

--- prefect/deploy.py
+++ prefect/deploy.py
@@ -1,15 +1,20 @@
 from flow import example_flow
 from prefect.docker import DockerImage
+import os
 
 def main():
+    registry = os.getenv('DOCKER_REGISTRY');
+
+    if not registry:
+        raise ValueError('Missing env var DOCKER_REGISTRY')
+
     example_flow.deploy(
         name="poc-example",
         work_pool_name="aws-push-pool",
         image=DockerImage(
-            name="poc-example:latest",
+            name=f"{registry}/prefect-flows:latest",
             dockerfile="Dockerfile",
-            platform="linux/amd64",
-        )
+        ),
     )
 
 
--- prefect/invoke.py
+++ prefect/invoke.py
@@ -4,7 +4,7 @@ from prefect.deployments import run_deployment
 
 
 def main():
-    run_deployment("example_flow/poc-example", parameters={"message": "Hello, Prefect!"})
+    run_deployment("example-flow/poc-example", parameters={"message": "Hello, Prefect!"})
 
 
 if __name__ == "__main__":
