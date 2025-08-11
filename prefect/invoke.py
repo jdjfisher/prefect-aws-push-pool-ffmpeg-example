57472d770bd8a6d0692119100b32f1d8ab94bd1d
Jordan Fisher <jordan.fisher@vivedia.net>
2025-08-11 11:03:07 +0100

    Updated invoke script

--- prefect/invoke.py
+++ prefect/invoke.py
@@ -1,10 +1,16 @@
 
 from prefect import flow
 from prefect.deployments import run_deployment  
-
+import sys
 
 def main():
-    run_deployment("example-flow/poc-example", parameters={"message": "Hello, Prefect!"})
+    if len(sys.argv) != 2:
+        print("Usage: python invoke.py <message>")
+        sys.exit(1)
+
+    message = sys.argv[1]
+
+    run_deployment("example-flow/poc-example", parameters={"message": message})
 
 
 if __name__ == "__main__":
