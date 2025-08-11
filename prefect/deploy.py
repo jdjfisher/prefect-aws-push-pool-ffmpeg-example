925b93e31fa8313953e89a983eb9d9fc1b3af8d3
Jordan Fisher <jordan.fisher@vivedia.net>
2025-08-11 13:57:13 +0100

    Platform build issue patch

--- prefect/deploy.py
+++ prefect/deploy.py
@@ -1,6 +1,7 @@
 from flow import example_flow
 from prefect.docker import DockerImage
 import os
+import subprocess
 
 def main():
     registry = os.getenv('DOCKER_REGISTRY');
@@ -8,13 +9,22 @@ def main():
     if not registry:
         raise ValueError('Missing env var DOCKER_REGISTRY')
 
+    image = f"{registry}/prefect-flows:latest"
+
+    # Manually running buildx for amd64 
+    subprocess.run(
+        ["docker", "buildx", "build", '--platform', "linux/amd64", "-t", image, ".", '--push'],
+        check=True,
+    )
+
     example_flow.deploy(
         name="poc-example",
         work_pool_name="aws-push-pool",
         image=DockerImage(
-            name=f"{registry}/prefect-flows:latest",
+            name=image,
             dockerfile="Dockerfile",
         ),
+        build=False,
     )
 
 
