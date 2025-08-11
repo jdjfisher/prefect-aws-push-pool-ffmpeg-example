5d049db8d98a3f418c88427c3c715529e882fd50
Jordan Fisher <jordan.fisher@vivedia.net>
2025-08-11 13:51:24 +0100

    Print ffmpeg version

--- prefect/flow.py
+++ prefect/flow.py
@@ -1,7 +1,9 @@
 from prefect import flow
-import os
+import subprocess
 
 @flow(log_prints=True, timeout_seconds=60)
 async def example_flow(message: str = "Hello, World!"):
     print(message)
-    os.system("ffmpeg -version")
+    result = subprocess.run("ffmpeg -version", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    version = result.stdout.decode('utf-8')
+    print(f"FFmpeg version: {version}")
